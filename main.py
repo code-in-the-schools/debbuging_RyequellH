e5326c1e93f6f2703ae378d9210115fe5da0302d
CodingQuell <ryequellhunter@gmail.com>
2020-07-21 17:41:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#Ste= p 1
+#{}Debug the following program so that it properly takes a users order and outputs the price of the order.
+#Step 2
+#Edit the existing code and have it print out the items you are ordering as follows...
+#Example
+# "Coffee x 0"
+# "Cake x 2"
+# "Tea x 3"
+
+#Step 3
+#Have the items you are ordering al
+Price = 0
+for i in listOfOrders:
+ message = str(input("would you like to buy a" ". Yes / No   ")
+if myOrder == str(input(message))
+myOrder == Yes and i = 1:
+    number = input("How many would you like? ")
+    for a in range(number)
+      price = price + 2.5
+
+  if myOrder == "Yes" and i = 2:
+    number = input("How many would you like? ")
+    For a in range(number):
+      price += 5.2so output their indavidual cost.
+#Example
+# "Coffee x 0 : 0.00"
+# "Cake x 2 : 5.00"
+# "Tea x 3 : 3.6"
+
+
+listOfOrders = ["Cofee" "Cake" "Tea"]
+
+  if myOrder == yes and i = 3:
+    number = int(input(
